fd50d76128bf2d33768ea4e19a58b356dd87ed3d
Scott Robbin <srobbin@gmail.com>
2013-03-23 10:14:52 -0500

    Tweet examples select field, and character count.

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,21 @@
 //= require active_admin/base
+
+$(function () {
+
+  var $reply = $("#tweet_reply"),
+      maxLength = $reply.attr("maxlength"),
+      $remaining = $(".remaining_characters span").text(maxLength);
+
+  // Use the example tweets to populate the actual reply textbox
+  $(".active_admin .example-replies").change(function () {
+    var $select = $(this),
+        reply = $select.val();
+
+    $reply.val(reply).trigger("keyup");
+  });
+
+  // Calculate the remaining characters
+  $reply.keyup(function () {
+    $remaining.text( maxLength - $reply.val().length );
+  });
+});
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,6 +5,17 @@ class Tweet < ActiveRecord::Base
   validates :tweet_id, :uniqueness => true
   after_save :send_reply
 
+  # Public: Sample replies
+  EXAMPLE_REPLIES = [
+    "Sorry to hear you're sick. We can help you by clicking on this link to file a report.",
+    "Sorry to hear you're ill. The Chicago health department can help.",
+    "Sorry to hear you're ill. The City can help by",
+    "That doesn't sound good. Let us help you here:",
+    "Where did you eat? Please let us know here:",
+    "That doesn't sound good. This might help:",
+    "Where did you eat? Report it!:"
+  ]
+
   private
     def send_reply
       if self.reply_changed? and !self.reply.blank?
