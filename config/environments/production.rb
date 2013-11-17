0eca15d356a917a7428d2a200bebabb05d74ee43
Scott Robbin <scott@robbin.co>
2013-11-17 15:53:51 -0600

    Needed settings for Heroku assets precompilation.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Foodpoison::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( html5shiv.js active_admin.css active_admin.js active_admin/print.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
