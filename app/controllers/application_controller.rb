0c5a1b3a8627a4be2496ef43a389b9b242f90aa7
Scott Robbin <scott@robbin.co>
2014-06-16 14:16:29 -0500

    Changed domain to www.foodbornechicago.org.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'www.foodbornechicago.org'
+
+private
+  def ensure_domain
+    if Rails.env.production? and request.host != APP_DOMAIN
+      redirect_to "https://#{APP_DOMAIN}#{request.fullpath}", :status => 301
+    end
+  end
 end
